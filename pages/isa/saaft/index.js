7db63456af359a02aaad772fcdb66ba0a97e59a0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-04-13 10:59:08 +0100

    Fix passHref

--- pages/isa/saaft/index.js
+++ pages/isa/saaft/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './Saaft.module.css';
 
 export default function Saaft() {
@@ -8,30 +9,36 @@ export default function Saaft() {
       </div>
 
       <section className={styles.worksContainer}>
-        <a className={styles.work} href='/isa/saaft/fc3'>
-          <h2>FC#3</h2>
-          <h3 className={styles.title}>Silvopasture in the Caatinga Biome of Brazil</h3>
-          <p className={styles.description}>Flipped Classroom #3</p>
-          <p className={styles.credits}>May 2020 • Márcia Figueira + João de Vasconcelos</p>
-        </a>
-        <a className={styles.work} href='/isa/saaft/fc2'>
-          <h2>FC#2</h2>
-          <h3 className={styles.title}>Asia and South America</h3>
-          <p className={styles.description}>
-            Flipped Classroom #2
-            <br />
-            Multi-level socioecological drivers of agrarian change.
-            <br />
-            Cultivating the dry forests of South America
-          </p>
-          <p className={styles.credits}>May 2020 • Márcia Figueira + João de Vasconcelos</p>
-        </a>
-        <a className={styles.work} href='/isa/saaft/fc1'>
-          <h2>FC#1</h2>
-          <h3 className={styles.title}>The Groundnut Basin in Senegal</h3>
-          <p className={styles.description}>Flipped Classroom #1</p>
-          <p className={styles.credits}>March 2020 • Márcia Figueira + João de Vasconcelos</p>
-        </a>
+        <Link href='/isa/saaft/fc3'>
+          <a className={styles.work}>
+            <h2>FC#3</h2>
+            <h3 className={styles.title}>Silvopasture in the Caatinga Biome of Brazil</h3>
+            <p className={styles.description}>Flipped Classroom #3</p>
+            <p className={styles.credits}>May 2020 • Márcia Figueira + João de Vasconcelos</p>
+          </a>
+        </Link>
+        <Link href='/isa/saaft/fc2'>
+          <a className={styles.work}>
+            <h2>FC#2</h2>
+            <h3 className={styles.title}>Asia and South America</h3>
+            <p className={styles.description}>
+              Flipped Classroom #2
+              <br />
+              Multi-level socioecological drivers of agrarian change.
+              <br />
+              Cultivating the dry forests of South America
+            </p>
+            <p className={styles.credits}>May 2020 • Márcia Figueira + João de Vasconcelos</p>
+          </a>
+        </Link>
+        <Link href='/isa/saaft/fc1'>
+          <a className={styles.work}>
+            <h2>FC#1</h2>
+            <h3 className={styles.title}>The Groundnut Basin in Senegal</h3>
+            <p className={styles.description}>Flipped Classroom #1</p>
+            <p className={styles.credits}>March 2020 • Márcia Figueira + João de Vasconcelos</p>
+          </a>
+        </Link>
       </section>
     </main>
   );
