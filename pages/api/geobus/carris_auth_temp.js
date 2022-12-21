98d4bad8d46bed64978b7f2429c9cff22332fb2a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-12-21 01:54:19 +0000

    Create carris_auth_temp.js
    Temporary solution for development purposes. Not intended for production.

---
+++ pages/api/geobus/carris_auth_temp.js
@@ -0,0 +1,32 @@
+/* * */
+/* RETURN CARRIS API CREDENTIALS */
+/* This route returns updated credentials for Carris API, */
+/* to be used by the GeoBus app, avoiding the delay of an App Store submission. */
+/* * */
+
+export default async function carrisApiCredentials(req, res) {
+  const carris = {
+    api_key: process.env.CARRIS_FIREBASE_API_KEY,
+    ios_app_id: process.env.CARRIS_IOS_APP_ID,
+  };
+  const fbrcUrl = `https://firebaseremoteconfig.googleapis.com/v1/projects/carris-planeador-de-viagem/namespaces/firebase:fetch?key=${carris.api_key}`;
+  let opt = {
+    method: 'POST',
+    body: JSON.stringify({
+      app_instance_id: 'id',
+      app_id: carris.ios_app_id,
+      app_build: '47.0'
+    }),
+  };
+  const fbrcRes = await fetch(fbrcUrl, opt);
+  const json = await fbrcRes.json();
+  return new Promise((resolve, reject) => {
+    res.statusCode = 200;
+    res.send({
+      endpoint: 'gateway.carris.pt',
+      token: json.entries.apikey_ios,
+      type: 'apikey',
+    });
+    resolve();
+  });
+}
