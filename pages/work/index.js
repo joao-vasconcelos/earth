6413444c2d5b6e43ff2e8e353e7e0edbaa628014
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-06-06 03:31:21 +0100

    Prettier format

--- pages/work/index.js
+++ pages/work/index.js
@@ -30,39 +30,17 @@ export default function Work() {
           <h1>It is very easy to be different, but very difficult to be better.</h1>
           <h3>Jonathan Ive</h3>
           <p>
-            Building better tools is what wakes me up in the morning. It all starts by defining the problem to be
-            solved, and thinking about it from the user&apos;s perspective. Only by focusing hard on this approach it is
-            possible to be confident in one&apos;s decisions. The following examples combine design, technology and a
-            lot of dedication to create intuitive tools that simplify it&apos;s users lives.
+            Building better tools is what wakes me up in the morning. It all starts by defining the problem to be solved, and thinking about it from the user&apos;s perspective. Only by focusing hard on this approach it is possible to be confident in
+            one&apos;s decisions. The following examples combine design, technology and a lot of dedication to create intuitive tools that simplify it&apos;s users lives.
           </p>
         </Container>
       </Intro>
 
       <ProjectsList>
-        <ProjectButton
-          id='spg'
-          title='Sociedade Portuguesa de Ginecologia'
-          tags={['Strategy', 'Brand Design', 'UI/UX Design', 'Platform Development']}
-          accent='rgb(203, 0, 90)'
-        />
-        <ProjectButton
-          id='register'
-          title='POS Register'
-          tags={['UI/UX Design', 'Platform Development']}
-          accent='#e42528'
-        />
-        <ProjectButton
-          id='geobus'
-          title='GeoBus'
-          tags={['UI/UX Design', 'App Development']}
-          accent='rgb(255, 160, 0)'
-        />
-        <ProjectButton
-          id='jodel'
-          title='Jodel'
-          tags={['Strategy', 'Brand Design', 'Web Development']}
-          accent='rgb(255, 100, 0)'
-        />
+        <ProjectButton id='spg' title='Sociedade Portuguesa de Ginecologia' tags={['Strategy', 'Brand Design', 'UI/UX Design', 'Platform Development']} accent='rgb(203, 0, 90)' />
+        <ProjectButton id='register' title='POS Register' tags={['UI/UX Design', 'Platform Development']} accent='#e42528' />
+        <ProjectButton id='geobus' title='GeoBus' tags={['UI/UX Design', 'App Development']} accent='rgb(255, 160, 0)' />
+        <ProjectButton id='jodel' title='Jodel' tags={['Strategy', 'Brand Design', 'Web Development']} accent='rgb(255, 100, 0)' />
         <ProjectButton id='jfld' title='Jean-François Le Dû' tags={['Web Development']} accent='rgb(170, 0, 215)' />
         <ProjectButton id='alh' title='Aroeira Lisbon Hotel' tags={['Web Development']} accent='#0a2341' />
         <ProjectButton id='lrf' title='Leal Rios Foundation' tags={['Web Development']} accent='rgb(71, 69, 85)' />
