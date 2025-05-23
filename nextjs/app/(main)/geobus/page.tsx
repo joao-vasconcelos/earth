d45987ae75965655677dd8909cacdaa28a265be6
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-05-23 17:48:00 +0000

    Redirect to root

--- nextjs/app/(main)/geobus/page.tsx
+++ nextjs/app/(main)/geobus/page.tsx
@@ -1,9 +1,11 @@
 /* * */
 
 import { GeoBusPage } from '@/components/geobus/GeoBusPage';
+import { redirect } from 'next/navigation';
 
 /* * */
 
 export default function Page() {
+	redirect('/');
 	return <GeoBusPage />;
 }
